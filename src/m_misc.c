7c506daca33d7cc6ae26458c961d4c557c704ef5
scooterpsu <3433982+scooterpsu@users.noreply.github.com>
2019-10-05 11:35:14 -0400

    Load prboom.wad outside of wadfiles array
    So it doesn't end up in config files

--- src/m_misc.c
+++ src/m_misc.c
@@ -1173,7 +1173,6 @@ void M_LoadDefaultsFile (char *file, boolean basedefault)
   //jff 3/4/98 redundant range checks for hud deleted here
   /* proff 2001/7/1 - added prboom.wad as last entry so it's always loaded and
      doesn't overlap with the cfg settings */
-  wad_files[MAXLOADFILES-1]="prboom.wad";
 }
 
 //
