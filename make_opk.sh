a9ee41bc0f58e225696c724851254408bf9d3280
scooterpsu <3433982+scooterpsu@users.noreply.github.com>
2019-10-14 09:02:52 -0400

    Fix exec path in opk .desktop files, add link for modlaunch

--- make_opk.sh
+++ make_opk.sh
@@ -9,7 +9,7 @@ cat > default.gcw0.desktop <<EOF
 [Desktop Entry]
 Name=CZDoom
 Comment=Limit-removing Doom Engine
-Exec=prboom -iwad $f
+Exec=czdoom -iwad \$f
 Terminal=false
 Type=Application
 StartupNotify=true
@@ -17,9 +17,26 @@ Icon=czdoom
 Categories=games;
 EOF
 
+# create modlaunch.gcw0.desktop
+cat > modlaunch.gcw0.desktop <<EOF
+[Desktop Entry]
+Name=CZDoom Mods
+Comment=CZDoom Mod Launcher
+Exec=czdoom-modlaunch \$f
+Terminal=false
+Type=Application
+StartupNotify=true
+Icon=czdoom-modlaunch
+Categories=games;
+EOF
+
+# copy/rename binary
+cp src/prboom czdoom
+
 # create opk
-FLIST="src/prboom"
+FLIST="czdoom"
 FLIST="${FLIST} default.gcw0.desktop"
+FLIST="${FLIST} modlaunch.gcw0.desktop"
 FLIST="${FLIST} src/distrib/data/*"
 
 rm -f ${OPK_NAME}
@@ -27,3 +44,9 @@ mksquashfs ${FLIST} ${OPK_NAME} -all-root -no-xattrs -noappend -no-exports
 
 cat default.gcw0.desktop
 rm -f default.gcw0.desktop
+
+cat modlaunch.gcw0.desktop
+rm -f modlaunch.gcw0.desktop
+
+# delete copied binary
+rm -f czdoom
